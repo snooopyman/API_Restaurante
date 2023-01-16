6c2df0c28c534586131ac15129ebd372e2307a17
Snooopyman <armando.snoop@gmail.com>
2023-01-16 16:54:26 +0100

    retoques de la api

---
+++ README.md
@@ -0,0 +1,17 @@
+# MVC_API Restaurant
+
+###  API for a restaurant
+
+
+
+### Project setup
+#### Clone the repository
+````
+git clone http://github.com/snooopyman/API_Restaurante
+````
+
+#### Install dependences
+```
+npm install 
+```
+
--- src/public/js/cliente.js
+++ src/public/js/cliente.js
@@ -1,8 +1,12 @@
 setInterval(async () => {
-    const textos = document.querySelectorAll("h1")
-    const data = await (await fetch("/api/")).json()
+    try {
+        const textos = document.querySelectorAll("h1")
+        const data = await (await fetch("/api/")).json()
 
-    textos.forEach(el => {
-        data.find(plato => el.id == plato.id).estado == "Activo" ? el.classList.add("color-red") : el.classList.remove("color-red")
-    })
-}, 30000)
+        textos.forEach(el => {
+            data.find(plato => el.id == plato.id).estado == "Inactivo" ? el.classList.add("color-red") : el.classList.remove("color-red")
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}, 5000)
